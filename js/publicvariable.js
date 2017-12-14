b57d4f47564c3715b91374aa92d639ae9b4ba068
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2017-12-14 22:10:27 +0700

    initiate commit

---
+++ js/publicvariable.js
@@ -0,0 +1,10 @@
+var imageslist = [
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/akad.png',
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/hostbin.png',
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/prowire.png',
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/samurai.png',
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/akad.png',
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/hostbin.png',
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/prowire.png',
+  '/home/ridwanzal/Web_Project/EdoWeb/public_html/myweb/img/work/samurai.png',
+];
