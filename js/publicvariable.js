a3ed9787e7284764fd11e583234d6de25e974e40
M. Ridwan Zalbina <zalbinaridwan@gmail.com>
2017-12-18 22:05:55 +0700

    commit all in 18 dec 2017

--- js/publicvariable.js
+++ js/publicvariable.js
@@ -23,3 +23,48 @@ var globalRes = {
 
 var idToOverlay = '.galhover';
 var classOverlay = 'overlay';
+
+// this is related
+var page = {
+  gotop : function(element, buttonclick){
+    element = $(element);
+    $window.scroll(function(){
+      if($(this).scrollTop() > 500){
+        element.fadeIn(100);
+      }else{
+        element.fadeOut(300);
+      }
+    });
+    element.click(function(){
+      $("html, body").animate({
+        scrollTop : 0
+      }, 800);
+      return false;
+    });
+  },
+  refresh : function(){
+    location.reload();
+  },
+  opennew : function(){
+    // using window.opener() method
+  }, 
+  // never use this module
+  gotoptwo : function(element, buttonclick){
+    var offset = 300,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        element = $(element);
+        $window.scroll(function(){
+          ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+          if ($(this).scrollTop() > offset_opacity) {
+              $back_to_top.addClass('cd-fade-out');
+          }
+        });
+        element.click(function(){
+          event.preventDefault();
+          $('body, html').animate({
+            scrollTop : 0
+          }, scroll_top_duration);
+        });
+  }
+}
